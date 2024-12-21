04f2d34069b8e41fd97fbba4f38c20491d723759
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-12-21 12:18:55 +0530

    :zap: code formatting

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,30 +1,53 @@
-import express from 'express';
-import { isAdmin, requireSignIn } from '../middleware/authMiddleware.js';
-import { createProductController, deleteProductController, filterProductsController, getProductController, getAllProductsController, getProductPhotoController, updateProductController } from '../controller/productController.js';
-import formidable from 'express-formidable';
+import express from "express";
+import { isAdmin, requireSignIn } from "../middleware/authMiddleware.js";
+import {
+  createProductController,
+  deleteProductController,
+  filterProductsController,
+  getProductController,
+  getAllProductsController,
+  getProductPhotoController,
+  updateProductController,
+} from "../controller/productController.js";
+import formidable from "express-formidable";
 
 const router = express.Router();
 
-
 // create product
-router.post("/create-product", requireSignIn, isAdmin, formidable(), createProductController);
+router.post(
+  "/create-product",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  createProductController
+);
 
 // get product
 router.get("/get-product/:slug", getProductController);
 
 // get all products
-router.get('/all-products', getAllProductsController);
+router.get("/all-products", getAllProductsController);
 
 // get photo
-router.get('/get-product-photo/:photoId', getProductPhotoController);
+router.get("/get-product-photo/:photoId", getProductPhotoController);
 
 // update product
-router.put("/update-product/:productId", requireSignIn, isAdmin, formidable(), updateProductController)
+router.put(
+  "/update-product/:productId",
+  requireSignIn,
+  isAdmin,
+  formidable(),
+  updateProductController
+);
 
 // delete product
 router.delete("/delete-product/:id", deleteProductController);
 
 // filter produts
-router.post("/filter-products", filterProductsController)
+router.post("/filter-products", filterProductsController);
 
-export default router
+// count products
+
+router.get("/product-count", productCountController);
+
+export default router;
